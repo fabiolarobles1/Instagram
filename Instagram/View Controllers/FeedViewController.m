03a5bee1282a7c72aa3225e01418de83ac9f50ff
Fabiola E. Robles Vega <fabiolarobles19@fb.com>
2020-07-07 00:13:22 -0400

    user can logout

---
+++ Instagram/View Controllers/FeedViewController.m
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.m
+//  Instagram
+//
+//  Created by Fabiola E. Robles Vega on 7/7/20.
+//  Copyright © 2020 Fabiola E. Robles Vega. All rights reserved.
+//
+
+#import "FeedViewController.h"
+#import <Parse/Parse.h>
+
+@interface FeedViewController ()
+
+@end
+
+@implementation FeedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        NSLog(@"Successfully logged out user.");
+        [self performSegueWithIdentifier:@"toLoginSegue" sender:nil];
+        
+    }];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
